e57530667f53a261648547b316a81907f6e17ff7
Simone Ivan Conte <sic2@st-andrews.ac.uk>
2014-04-02 01:25:54 +0100

    basicactions does speechoutput

--- HaptiQ/HaptiQ_API/Actions/BasicAction.cs
+++ HaptiQ/HaptiQ_API/Actions/BasicAction.cs
@@ -7,9 +7,17 @@ namespace HaptiQ_API
 {
     public class BasicAction : IAction
     {
+        private String _information;
+
+        public BasicAction(String information)
+        {
+            _information = information;
+        }
+
         public void run(uint id, Dictionary<int, double> pressureData)
         {
             Console.WriteLine("Getting input from device with id " + id);
+            SpeechOutput.Instance.speak(_information);
         }
     }
 }
