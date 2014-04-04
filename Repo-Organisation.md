471d59f1825dcc96716b7821ede80cf691718eb0
Simone Ivan Conte <sic2@st-andrews.ac.uk>
2014-04-04 15:47:18 +0100

    repo organisation:
    - repo organisation markdown file
    - renamed folder of GraphsVisualiser app
    - created a simple template app to help beginners to get started

---
+++ Repo-Organisation.md
@@ -0,0 +1,32 @@
+# Repository organisation
+
+## HaptiQ 
+
+This folder contains:
+	- the HaptiQ API.
+	- the Input API.
+	- two Example applications (graph visualiser and function visualiser).
+	- a test application used for developing and debugging.
+	- a small test suite
+	- external resources
+	- example of configuration files
+	
+# HaptiQ 3D Models
+
+3D Models for both the 4-HaptiQ and the 8-HaptiQ.
+
+# Templates
+
+Contain example applications using the HaptiQ. These are useful as starting points for new applications.
+
+# Other
+
+Contain work in progress projects and experimental code. 
+
+# Manual
+
+Contain a manual useful to get start with the HaptiQ
+
+# Dissertation
+
+Contains the Latex and PDF files of the dissertation as written by Simone Ivan Conte, University of St Andrews (04/04/2014) 
---
+++ Templates/SurfaceApplication1/TestApp/CustomShape.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Input_API;
+using HaptiQ_API;
+using System.Windows.Media;
+
+namespace TestApp
+{
+    /// <summary>
+    /// This class is identical to HapticLine, but it shows that HapticShape (and other classes)
+    /// can be extended easily. 
+    /// </summary>
+    public class CustomShape : HapticShape
+    {
+        private Tuple<Point, Point> _pair;
+
+        /// <summary>
+        /// Constructor accepting System.Windows Points
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="w"></param>
+        public CustomShape(System.Windows.Point v, System.Windows.Point w)
+        {
+            _pair = new Tuple<Point, Point>(new Point(v.X, v.Y), new Point(w.X, w.Y));
+            this.geometry = new LineGeometry(_pair.Item1.toSysWinPoint(), _pair.Item2.toSysWinPoint());
+        }
+
+        /// <summary>
+        /// Set the color of the HapticLine.
+        /// Also automatically set the thickness of the HapticLine to be 20.
+        /// </summary>
+        /// <param name="brush"></param>
+        public override void color(Brush brush)
+        {
+            this.Stroke = brush;
+        }
+
+        /// <summary>
+        /// Return true if point is in the HapticLine
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        protected override bool pointIsInside(Point point)
+        {
+            return pointIsCloseToSegment(point, _pair.Item1, _pair.Item2, NEARNESS_TOLLERANCE);
+        }
+
+        /// <summary>
+        /// Returns true if a given point is near a segment.
+        /// Play sound, changing duration when getting further away.
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="startLine"></param>
+        /// <param name="endLine"></param>
+        /// <param name="TOLLERANCE"></param>
+        /// <returns></returns>
+        protected override bool pointIsCloseToSegment(Point point, Point startLine, Point endLine, double TOLLERANCE)
+        {
+            double dst = distancePointToSegment(point, startLine, endLine);
+            int duration = - (int) dst * ((BeepOutput.MIN_DURATION - BeepOutput.MAX_DURATION) / (int) TOLLERANCE);
+            if (dst <= TOLLERANCE)
+            {
+                BeepOutput.Beep(duration);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns an edge-corner behaviour based on the position of the HaptiQ
+        /// on the HapticLine
+        /// </summary>
+        /// <param name="haptiQ"></param>
+        /// <returns></returns>
+        protected override IBehaviour chooseBehaviour(HaptiQ haptiQ)
+        {
+            List<Tuple<Point, Point>> lines = new List<Tuple<Point, Point>>();
+            lines.Add(_pair);
+            return new EdgeCornerBehaviour(haptiQ, lines);
+        }
+
+        /// <summary>
+        /// Return the two end points of this haptic line
+        /// </summary>
+        /// <returns></returns>
+        public Tuple<Point, Point> getEndPoints()
+        {
+            return _pair;
+        }
+
+        /// <summary>
+        /// Override OnRender to display tollerance borders of the shape
+        /// </summary>
+        /// <param name="drawingContext"></param>
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            drawingContext.DrawEllipse(null, new Pen(Brushes.Red, 1.0),
+                _pair.Item1.toSysWinPoint(), NEARNESS_TOLLERANCE, NEARNESS_TOLLERANCE);
+            drawingContext.DrawEllipse(null, new Pen(Brushes.Red, 1.0),
+                _pair.Item2.toSysWinPoint(), NEARNESS_TOLLERANCE, NEARNESS_TOLLERANCE);
+         }
+    }
+}
